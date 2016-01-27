0a7381a66471c2d491e904039b1a8724ec186800
Dave Owens <dwowens@thenavisway.com>
2016-01-26 16:49:51 -0800

    Story ID: Summary: - moved untangled.server.impl.database (from u-server) to untangled.datomic.impl - moved datomic-specific functions from u-server.core to untangled.datomic.core - moved specs
    Comments to QA/Testers:
    Changelog:

--- project.clj
+++ project.clj
@@ -1,6 +1,29 @@
-(defproject untangled-datomic "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+(defproject navis/untangled-datomic "0.1.0-SNAPSHOT"
+
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [datomic-helpers "1.0.0"]
+                 [com.datomic/datomic-pro "0.9.5206" :exclusions [joda-time]]
+                 [untangled-server "0.4.0-SNAPSHOT"]
+                 [com.stuartsierra/component "0.2.3"]
+                 [untangled-spec "0.3.1" :scope "test" :exclusions [io.aviso/pretty]]
+                 ]
+
+
+  :repositories [["releases" "https://artifacts.buehner-fry.com/artifactory/internal-release"]
+                 ["third-party" "https://artifacts.buehner-fry.com/artifactory/internal-3rdparty"]
+                 ["snapshots" "https://artifacts.buehner-fry.com/artifactory/internal-snapshots"]]
+
+  :deploy-repositories [["releases" {:url           "https://artifacts.buehner-fry.com/artifactory/internal-release"
+                                     :snapshots     false
+                                     :sign-releases false}]
+                        ["snapshots" {:url           "https://artifacts.buehner-fry.com/artifactory/internal-snapshots"
+                                      :sign-releases false}]]
+
+  :test-refresh {:report       untangled-spec.reporters.terminal/untangled-report
+                 :changes-only true}
+
+  :source-paths ["src" "checkouts/untangled-server/src"]
+  :test-paths ["specs"]
+  :resource-paths ["src" "resources"]
+
+  )

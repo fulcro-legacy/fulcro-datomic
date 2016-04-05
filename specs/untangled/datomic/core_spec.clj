bdadcaeb14a659fecc24eae172a4055249fce85c
Ethan Gracer <EDGracer@thenavisway.com>
2016-04-05 12:11:50 -0700

    Story ID: develop Changelog: - Added `core/query` function that calls datomic.api/q with a database component, and resolves seeded ids for you

--- specs/untangled/datomic/core_spec.clj
+++ specs/untangled/datomic/core_spec.clj
@@ -1,10 +1,42 @@
 (ns untangled.datomic.core-spec
   (:require [untangled.datomic.core :as core]
             [untangled.datomic.schema :as schema]
+            [untangled.datomic.test-helpers :as test]
+            [untangled.datomic.protocols :as ud]
             [io.rkn.conformity :as c]
             [untangled-spec.core :refer [when-mocking specification provided behavior assertions]]
             [untangled.datomic.fixtures.migration-handling-mock-data :as md]
             [untangled.datomic.impl.util :as util]))
+
+(specification "The query function"
+  (test/with-db-fixture fixture
+    (let [username (core/query '[:find ?name . :in $ ?id :where
+                                 [?a :user/username ?name]
+                                 [?a :user/address ?id]]
+                     fixture
+                     :datomic.id/address)
+          zipcode (core/query '[:find ?zip . :in $ ?status :where
+                                [?a :address/zipcode ?zip]
+                                [?e :user/address ?a]
+                                [?e :user/status ?status]]
+                    fixture
+                    :user.status/pending)]
+
+      (assertions
+        "If bound variable is a seeded-id, resolves it to a real datomic id."
+        username => "untangled"
+
+        "If bound variable is not a seeded-id, leaves as is."
+        zipcode => 97702))
+
+    :migrations "sample-migrations.migrations.test"
+    :seed-fn (fn [conn] (test/link-and-load-seed-data
+                          conn [{:db/id           :datomic.id/address
+                                 :address/zipcode 97702}
+                                {:db/id         :datomic.id/user
+                                 :user/username "untangled"
+                                 :user/address  :datomic.id/address
+                                 :user/status   :user.status/pending}]))))
 
 (specification "main-handler"
   (provided "when passed the --migrate option with the 'all' keyword"

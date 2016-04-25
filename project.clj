fe0634df975a7926cab2b9191ffa590572cfad65
Tony Kay <twkay@thenavisway.com>
2016-04-25 08:30:15 -0700

    changelog/project for release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject navis/untangled-datomic "0.4.8-SNAPSHOT"
+(defproject navis/untangled-datomic "0.4.8"
   :description "Datomic plugin for Untangled webapps"
   :url "http://www.github.com/untangled-web"
   :license {:name "MIT"

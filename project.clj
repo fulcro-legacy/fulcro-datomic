0ebe63c98cb2a54b13dbfc6e9bf08fb062e820a3
Tony Kay <tony.kay@gmail.com>
2017-12-06 22:57:37 -0800

    version bump

--- project.clj
+++ project.clj
@@ -1,25 +1,30 @@
-(defproject fulcrologic/fulcro-datomic "1.0.0-SNAPSHOT"
+(defproject fulcrologic/fulcro-datomic "2.0.0-alpha1"
   :description "Datomic plugin for Fulcro webapps"
   :url "http://www.github.com/fulcro-web"
   :min-lein-version "2.7.0"
   :license {:name "MIT"
             :url  "https://opensource.org/licenses/MIT"}
 
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [datomic-helpers "1.0.0"]
-                 [com.datomic/datomic-free "0.9.5561" :scope "provided" :exclusions [joda-time]]
+                 [com.datomic/datomic-free "0.9.5561" :scope "provided" #_#_:exclusions [joda-time]]
                  [org.clojure/math.combinatorics "0.1.1"]
                  [com.stuartsierra/component "0.3.2"]
                  [org.clojure/tools.namespace "0.3.0-alpha4"]
                  [org.clojure/tools.cli "0.3.5"]
                  [org.clojure/java.classpath "0.2.3"]
-                 [io.rkn/conformity "0.5.1" :exclusions [com.datomic/datomic-free]]
+                 [io.rkn/conformity "0.5.1"]
                  [com.taoensso/timbre "4.10.0"]
+                 [fulcrologic/fulcro-spec "2.0.0-beta3" :scope "test"]
+                 [democracyworks/datomic-toolbox "2.0.4" :exclusions [com.datomic/datomic-pro]]
+                 [datomic-schema-grapher "0.0.1"]
+
+                 ; pinned versions. remove and re-calculate if changing versions of anything above
                  [commons-codec "1.10"]
-                 [org.clojure/tools.reader "1.0.0"]
-                 [fulcrologic/fulcro-spec "1.0.0-beta3-SNAPSHOT" :scope "test" :exclusions [io.aviso/pretty com.google.guava/guava joda-time]]
-                 [democracyworks/datomic-toolbox "2.0.4" :exclusions [com.datomic/datomic-pro commons-logging commons-codec]]
-                 [datomic-schema-grapher "0.0.1"]]
+                 [commons-logging "1.2"]
+                 [com.google.guava/guava "20.0"]
+                 [org.clojure/tools.reader "1.1.0"]
+                 [joda-time "2.8.2"]]
 
   :plugins [[com.jakemccrary/lein-test-refresh "0.20.0"]]
 

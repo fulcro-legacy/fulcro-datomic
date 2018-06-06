262fb629fc250a8675afacf7b16ac876d47cb597
Tony Kay <tony.kay@gmail.com>
2018-06-05 20:41:35 -0700

    various updates to versions

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fulcrologic/fulcro-datomic "2.0.0-alpha1"
+(defproject fulcrologic/fulcro-datomic "2.0.0-beta1"
   :description "Datomic plugin for Fulcro webapps"
   :url "http://www.github.com/fulcro-web"
   :min-lein-version "2.7.0"
@@ -7,7 +7,7 @@
 
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [datomic-helpers "1.0.0"]
-                 [com.datomic/datomic-free "0.9.5561" :scope "provided" #_#_:exclusions [joda-time]]
+                 [com.datomic/datomic-free "0.9.5697" :scope "provided"]
                  [org.clojure/math.combinatorics "0.1.1"]
                  [com.stuartsierra/component "0.3.2"]
                  [org.clojure/tools.namespace "0.3.0-alpha4"]
@@ -15,16 +15,9 @@
                  [org.clojure/java.classpath "0.2.3"]
                  [io.rkn/conformity "0.5.1"]
                  [com.taoensso/timbre "4.10.0"]
-                 [fulcrologic/fulcro-spec "2.0.0-beta3" :scope "test"]
-                 [democracyworks/datomic-toolbox "2.0.4" :exclusions [com.datomic/datomic-pro]]
-                 [datomic-schema-grapher "0.0.1"]
-
-                 ; pinned versions. remove and re-calculate if changing versions of anything above
-                 [commons-codec "1.10"]
-                 [commons-logging "1.2"]
-                 [com.google.guava/guava "20.0"]
-                 [org.clojure/tools.reader "1.1.0"]
-                 [joda-time "2.8.2"]]
+                 [fulcrologic/fulcro-spec "2.1.0-1" :scope "test"]
+                 [democracyworks/datomic-toolbox "2.0.5" :exclusions [com.datomic/datomic-pro]]
+                 [datomic-schema-grapher "0.0.1"]]
 
   :plugins [[com.jakemccrary/lein-test-refresh "0.20.0"]]
 
